b4fa5f102e0100273fa4f9a6d69679aa1b75fca3
Anil Dinesh Kumar Dhanaraj <MY020429@amway.com>
2021-02-04 18:08:29 +0800

    adk123

--- src/POS_Registration/Registration.java
+++ src/POS_Registration/Registration.java
@@ -14,8 +14,14 @@ public class Registration
 		 System.setProperty("webdriver.chrome.driver", "C:\\libs\\chromedriver.exe");
                   ChromeDriver driver = new ChromeDriver();
                   driver.get("https://pos.uat4.amway.com.ph/registration");
+
                   driver.get("https://pos.uat4.amway.com.ph/registration");
+                  driver.get("https://pos.uat4.amway.com.ph/registration");
+
                   driver.manage().window().maximize();
+                  driver.manage().window().maximize();
+                  driver.manage().window().maximize();
+                  
                   
                   
          Thread.sleep(10000);
